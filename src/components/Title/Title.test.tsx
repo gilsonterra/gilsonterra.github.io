b6d489f02700a1c75190d76675f472cdf7892b09
Gilson Terra <gilson.oliveira@grupoboticario.com.br>
2025-01-03 19:29:05 -0300

    chore: ajuste no prettier

--- src/components/Title/Title.test.tsx
+++ src/components/Title/Title.test.tsx
@@ -1,12 +1,12 @@
 import { describe } from "node:test";
-import { render, screen} from '@testing-library/react'
+import { render, screen } from "@testing-library/react";
 
 import Title from "./Title";
 
-describe('Teste componente Title', () => {
-    test('Deve renderizar o componente Title', () => {
-        render(<Title />)
+describe("Teste componente Title", () => {
+  test("Deve renderizar o componente Title", () => {
+    render(<Title />);
 
-        expect(screen.getByText('Gilson Terra')).toBeInTheDocument()
-    })
-})
+    expect(screen.getByText("Gilson Terra")).toBeInTheDocument();
+  });
+});
--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,5 +1,5 @@
 const Title: React.FC = () => {
-    return <h1 className="font-light  text-4xl text-white">Gilson Terra</h1>;
-}
+  return <h1 className="font-light  text-4xl text-white">Gilson Terra</h1>;
+};
 
 export default Title;
